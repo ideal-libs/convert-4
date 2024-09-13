59f3375909dc8e96d010c0fd3660ab7a9c65910e
Daniel dos Santos Fonseca <danie.dos.s.fonseca@gmail.com>
2024-09-12 23:47:12 -0300

    feature: Add temperature

--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,2 @@
 export * from "./data-size";
+export * from "./temperature";
--- packages/temperature.ts
+++ packages/temperature.ts
@@ -1,125 +1,160 @@
-class Temperature {
-  private value: number;
-  private scale: string;
+export type FromTemperature = {
+  C?: number;
+  F?: number;
+  K?: number;
+  R?: number;
+  Re?: number;
+  Celsius?: number;
+  Fahrenheit?: number;
+  Kelvin?: number;
+  Rankine?: number;
+  Reaumur?: number;
+};
+export class Temperature {
+  private data: FromTemperature = {};
 
-  private constructor(value: number, scale: string) {
-    this.value = value;
-    this.scale = scale;
+  private validation(data: FromTemperature) {
+    const keys = Object.keys(data);
+    const validKeys = [
+      "C",
+      "F",
+      "K",
+      "R",
+      "Re",
+      "Celsius",
+      "Fahrenheit",
+      "Kelvin",
+      "Rankine",
+      "Reaumur",
+    ];
+    keys.forEach((key) => {
+      if (!validKeys.includes(key)) {
+        throw new Error(`Invalid key: ${key}`);
+      }
+    });
+  }
+
+  private constructor(data: FromTemperature) {
+    this.validation(data);
+    if (data.C) this.data.C = data.C;
+    if (data.F) this.data.F = data.F;
+    if (data.K) this.data.K = data.K;
+    if (data.R) this.data.R = data.R;
+    if (data.Re) this.data.Re = data.Re;
+    if (data.Celsius) this.data.C = data.Celsius;
+    if (data.Fahrenheit) this.data.F = data.Fahrenheit;
+    if (data.Kelvin) this.data.K = data.Kelvin;
+    if (data.Rankine) this.data.R = data.Rankine;
+    if (data.Reaumur) this.data.Re = data.Reaumur;
   }
 
   static fromCelsius(value: number): Temperature {
-    return new Temperature(value, "C");
+    return new Temperature({ C: value });
   }
 
   static fromFahrenheit(value: number): Temperature {
-    return new Temperature(value, "F");
+    return new Temperature({ F: value });
   }
 
   static fromKelvin(value: number): Temperature {
-    return new Temperature(value, "K");
+    return new Temperature({ K: value });
   }
 
   static fromRankine(value: number): Temperature {
-    return new Temperature(value, "R");
+    return new Temperature({ R: value });
   }
 
   static fromReaumur(value: number): Temperature {
-    return new Temperature(value, "Re");
+    return new Temperature({ Re: value });
   }
 
   toCelsius(): number {
-    switch (this.scale) {
-      case "C":
-        return this.value;
-      case "F":
-        return (this.value - 32) * (5 / 9);
-      case "K":
-        return this.value - 273.15;
-      case "R":
-        return (this.value - 491.67) * (5 / 9);
-      case "Re":
-        return this.value * (5 / 4);
-      default:
-        throw new Error(`Unknown scale: ${this.scale}`);
-    }
+    const values: FromTemperature = {
+      C: this.data.C || 0,
+      F: this.data.F ? (this.data.F - 32) * (5 / 9) : 0,
+      K: this.data.K ? this.data.K - 273.15 : 0,
+      R: this.data.R ? (this.data.R - 491.67) * (5 / 9) : 0,
+      Re: this.data.Re ? this.data.Re * (5 / 4) : 0,
+      Celsius: this.data.Celsius || 0,
+      Fahrenheit: this.data.Fahrenheit ? (this.data.Fahrenheit - 32) * (5 / 9) : 0,
+      Kelvin: this.data.Kelvin ? this.data.Kelvin - 273.15 : 0,
+      Rankine: this.data.Rankine ? (this.data.Rankine - 491.67) * (5 / 9) : 0,
+      Reaumur: this.data.Reaumur ? this.data.Reaumur * (5 / 4) : 0,
+    };
+
+    return Object.values(values).reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
   }
 
   toFahrenheit(): number {
-    switch (this.scale) {
-      case "C":
-        return this.value * (9 / 5) + 32;
-      case "F":
-        return this.value;
-      case "K":
-        return (this.value - 273.15) * (9 / 5) + 32;
-      case "R":
-        return this.value - 459.67;
-      case "Re":
-        return this.value * (9 / 4) + 32;
-      default:
-        throw new Error(`Unknown scale: ${this.scale}`);
-    }
+    const values: FromTemperature = {
+      C: this.data.C ? this.data.C * (9 / 5) + 32 : 0,
+      F: this.data.F || 0,
+      K: this.data.K ? (this.data.K - 273.15) * (9 / 5) + 32 : 0,
+      R: this.data.R ? this.data.R - 459.67 : 0,
+      Re: this.data.Re ? this.data.Re * (9 / 4) + 32 : 0,
+      Celsius: this.data.Celsius ? this.data.Celsius * (9 / 5) + 32 : 0,
+      Fahrenheit: this.data.Fahrenheit || 0,
+      Kelvin: this.data.Kelvin ? (this.data.Kelvin - 273.15) * (9 / 5) + 32 : 0,
+      Rankine: this.data.Rankine ? this.data.Rankine - 459.67 : 0,
+      Reaumur: this.data.Reaumur ? this.data.Reaumur * (9 / 4) + 32 : 0,
+    };
+
+    return Object.values(values).reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
   }
 
   toKelvin(): number {
-    switch (this.scale) {
-      case "C":
-        return this.value + 273.15;
-      case "F":
-        return (this.value - 32) * (5 / 9) + 273.15;
-      case "K":
-        return this.value;
-      case "R":
-        return this.value * (5 / 9);
-      case "Re":
-        return this.value * (5 / 4) + 273.15;
-      default:
-        throw new Error(`Unknown scale: ${this.scale}`);
-    }
+    const values: FromTemperature = {
+      C: this.data.C ? this.data.C + 273.15 : 0,
+      F: this.data.F ? (this.data.F - 32) * (5 / 9) + 273.15 : 0,
+      K: this.data.K || 0,
+      R: this.data.R ? this.data.R * (5 / 9) : 0,
+      Re: this.data.Re ? this.data.Re * (5 / 4) + 273.15 : 0,
+      Celsius: this.data.Celsius ? this.data.Celsius + 273.15 : 0,
+      Fahrenheit: this.data.Fahrenheit ? (this.data.Fahrenheit - 32) * (5 / 9) + 273.15 : 0,
+      Kelvin: this.data.Kelvin || 0,
+      Rankine: this.data.Rankine ? this.data.Rankine * (5 / 9) : 0,
+      Reaumur: this.data.Reaumur ? this.data.Reaumur * (5 / 4) + 273.15 : 0,
+    };
+
+    return Object.values(values).reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
   }
 
   toRankine(): number {
-    switch (this.scale) {
-      case "C":
-        return (this.value + 273.15) * (9 / 5);
-      case "F":
-        return this.value + 459.67;
-      case "K":
-        return this.value * (9 / 5);
-      case "R":
-        return this.value;
-      case "Re":
-        return this.value * (9 / 4) + 491.67;
-      default:
-        throw new Error(`Unknown scale: ${this.scale}`);
-    }
+    const values: FromTemperature = {
+      C: this.data.C ? (this.data.C + 273.15) * (9 / 5) : 0,
+      F: this.data.F ? this.data.F + 459.67 : 0,
+      K: this.data.K ? this.data.K * (9 / 5) : 0,
+      R: this.data.R || 0,
+      Re: this.data.Re ? this.data.Re * (9 / 4) + 491.67 : 0,
+      Celsius: this.data.Celsius ? (this.data.Celsius + 273.15) * (9 / 5) : 0,
+      Fahrenheit: this.data.Fahrenheit ? this.data.Fahrenheit + 459.67 : 0,
+      Kelvin: this.data.Kelvin ? this.data.Kelvin * (9 / 5) : 0,
+      Rankine: this.data.Rankine || 0,
+      Reaumur: this.data.Reaumur ? this.data.Reaumur * (9 / 4) + 491.67 : 0,
+    };
+    return Object.values(values).reduce((acc, curr) => acc + curr, 0);
   }
 
   toReaumur(): number {
-    switch (this.scale) {
-      case "C":
-        return this.value * (4 / 5);
-      case "F":
-        return (this.value - 32) * (4 / 9);
-      case "K":
-        return (this.value - 273.15) * (4 / 5);
-      case "R":
-        return (this.value - 491.67) * (4 / 9);
-      case "Re":
-        return this.value;
-      default:
-        throw new Error(`Unknown scale: ${this.scale}`);
-    }
-  }
+    const values: FromTemperature = {
+      C: this.data.C ? this.data.C * (4 / 5) : 0,
+      F: this.data.F ? (this.data.F - 32) * (4 / 9) : 0,
+      K: this.data.K ? (this.data.K - 273.15) * (4 / 5) : 0,
+      R: this.data.R ? (this.data.R - 491.67) * (4 / 9) : 0,
+      Re: this.data.Re || 0,
+      Celsius: this.data.Celsius ? this.data.Celsius * (4 / 5) : 0,
+      Fahrenheit: this.data.Fahrenheit ? (this.data.Fahrenheit - 32) * (4 / 9) : 0,
+      Kelvin: this.data.Kelvin ? (this.data.Kelvin - 273.15) * (4 / 5) : 0,
+      Rankine: this.data.Rankine ? (this.data.Rankine - 491.67) * (4 / 9) : 0,
+      Reaumur: this.data.Reaumur || 0,
+    };
 
-  toString(): string {
-    return `${this.value}°${this.scale}`;
+    return Object.values(values).reduce((acc, curr) => acc + curr, 0);
   }
 }
-
-// Example usage
-const tempC = Temperature.fromCelsius(25);
-console.log(tempC.toFahrenheit()); // 77
-console.log(tempC.toKelvin()); // 298.15
-console.log(tempC.toRankine()); // 536.67
-console.log(tempC.toReaumur()); // 20
